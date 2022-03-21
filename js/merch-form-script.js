a43670db09522050164af690e1b2e4bd912f21d8
Ingmar Daseville <ingdas@gmail.com>
2022-03-21 22:56:11 +0100

    Form toegevoegd

---
+++ js/merch-form-script.js
@@ -0,0 +1,56 @@
+$("#merchForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Heb je alles ingevuld?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#Mname").val();
+    var email = $("#Memail").val();
+    var phone = $("#Madres").val();
+    var msg_subject = "Pixels Website E-mail";
+    var message = $("#Mmessage").val();
+
+
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message + "&phone=" + phone,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    $("#merchForm")[0].reset();
+    submitMSG(true, "Bedankt voor je berichtje!")
+}
+
+function formError(){
+    $("#merchForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated groen";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#MmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
